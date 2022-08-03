85a06d91bafbc9ddb4d60ebea31f9bb1d6479278
Phil Bambridge <phil.bambridge@ons.gov.uk>
2022-08-03 10:26:05 +0100

    Correct DS download/unpack location

--- get_design_system.sh
+++ get_design_system.sh
@@ -4,5 +4,5 @@ set -e
 
 TMPFILE=`mktemp ./templates.XXXXXXXXXX`
 wget https://github.com/ONSdigital/design-system/releases/download/53.1.1/templates.zip -O $TMPFILE
-unzip -d ./ $TMPFILE
+unzip -d ./theme $TMPFILE
 rm $TMPFILE
