452935da0de0466dc21f1907e0f1bd91cbe9b86e
Phil Bambridge <phil.bambridge@ons.gov.uk>
2022-08-04 17:58:51 +0100

    Add paragraphng filter, apply to method desc

--- sml_builder/__init__.py
+++ sml_builder/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g, abort, escape
+from flask import Flask, render_template, request, g, abort, escape, Markup
 
 app = Flask(__name__)
 
@@ -16,3 +16,10 @@ import sml_builder.page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.template_filter("paras")
+def string_to_paragraph(value):
+    """Wraps passed string in <p> tags and converts newlines to <p> pairs"""
+    body = escape(value).replace("\n", Markup("</p><p>"))
+    return Markup(f"<p>{body}</p>")
