0318f0b0165e1c30bdddd9e4dc66d0b2cda1d835
Paul-Joel <paul.joel@ons.gov.uk>
2022-08-10 14:35:33 +0100

    Updated to make letter and related optional - letter is only needed if the term should use a different first letter

---
+++ content/glossary/markdown.jsonnet
@@ -0,0 +1,7 @@
+{
+  term: 'Markdown',
+  related: [],
+  meaning: |||
+    A popular and simple markup language used across the Web for formatting text.  Markdown is the format in which SML specifications are written.
+  |||
+}
--- sml_builder/glossary.py
+++ sml_builder/glossary.py
@@ -13,14 +13,16 @@ def display_glossary():
     glossary_dir = "./content/glossary"
     for file in listdir(glossary_dir):
         glossary_term = loads(evaluate_file(f"{glossary_dir}/{file}"))
+        letter_value = glossary_term["letter"].upper() if 'letter' in glossary_term \
+            else glossary_term["term"][0].upper()
         glossary_list.append({
             "term": glossary_term["term"],
             "hash": convert_term(glossary_term["term"]),
-            "letter": glossary_term["letter"].upper(),
-            "related": glossary_term["related"],
+            "letter": letter_value,
+            "related": glossary_term["related"] if 'related' in glossary_term else [],
             "meaning": glossary_term["meaning"]
         })
-        nav_options_list.append(glossary_term["letter"].upper())
+        nav_options_list.append(letter_value)
 
     nav_options = sorted(list(set(nav_options_list)))
     glossary = sorted(glossary_list, key=lambda d: d['term'])
