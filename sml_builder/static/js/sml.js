ad215f7017a296465f4ccfe94048a5ee40b128b7
Ben Latham <benlatham123@hotmail.co.uk>
2022-08-15 13:35:29 +0100

    tweaks to the top button

--- sml_builder/static/js/sml.js
+++ sml_builder/static/js/sml.js
@@ -2,9 +2,11 @@
 
 //Get the button
 var mybutton = document.getElementById("topBtn");
+// document.querySelector.footer
+var footer = document.footer
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -12,10 +14,5 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
+  console.log(mybutton.getBoundingClientRect().bottom)
 }
-
-// When the user clicks on the button, scroll to the top of the document
-document.getElementById("topBtn").addEventListener('click', () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
