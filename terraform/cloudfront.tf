07dff6a5a5008c6d15f95f984f77c818df8b4e69
Phil Bambridge <phil.bambridge@ons.gov.uk>
2022-08-16 01:43:26 +0100

    Simplify CF, re-add 404 handling

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -16,36 +16,24 @@ resource "aws_cloudfront_distribution" "sml-catalogue" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
+  custom_error_response {
+    error_code         = "403"
+    response_code      = "404"
+    response_page_path = "/page-not-found"
+  }
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "404"
+    response_page_path = "/page-not-found"
+  }
+
   logging_config {
     include_cookies = false
     bucket          = "statistical-methods-library-cf-logs.s3.amazonaws.com"
     prefix          = terraform.workspace
   }
 
-  # aliases = ["mysite.example.com", "yoursite.example.com"]
-
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "*"
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
@@ -59,13 +47,12 @@ resource "aws_cloudfront_distribution" "sml-catalogue" {
       }
     }
 
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-
+    viewer_protocol_policy     = "redirect-to-https"
     response_headers_policy_id = aws_cloudfront_response_headers_policy.noindex.id
+
+    min_ttl     = 0
+    default_ttl = 86400
+    max_ttl     = 31536000
   }
 
   price_class = "PriceClass_100"
