b2ce4c56a31103b4cdb2a6732accbd196fc758e4
Ben Latham <benlatham123@hotmail.co.uk>
2022-08-22 16:57:12 +0100

    use venv

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,8 @@
-apt-get install pyenv
-pip install pipenv
-pipenv sync --dev --python /usr/bin/python3
+echo "install pipenv"
+apt-get install pipenv
+echo "Make virtual environment"
+python3 -m venv venv
+echo "Pipenv sync"
+pipenv sync --dev
+echo "Install terraform"
 apt-get install terraform
