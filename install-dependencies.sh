c2d3a2fb040a3b3792f1d3a630791f468cce570e
Ben Latham <benlatham123@hotmail.co.uk>
2022-08-22 17:01:47 +0100

    sourc venv

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,6 +2,7 @@ echo "install pipenv"
 pip install pipenv
 echo "Make virtual environment"
 python3 -m venv venv
+source venv/bin/activate
 echo "Pipenv sync"
 pipenv sync --dev
 echo "Install terraform"
