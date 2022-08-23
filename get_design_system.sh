19abb34b4bb4144354f225fe7b8b4b11f44a273b
Ben Latham <benlatham123@hotmail.co.uk>
2022-08-23 16:05:28 +0100

    update design system an d remove patch (#14)
    Co-authored-by: Phil Bambridge <44780827+bambrp@users.noreply.github.com>

--- get_design_system.sh
+++ get_design_system.sh
@@ -3,8 +3,10 @@
 set -e
 
 TMPFILE=`mktemp ./templates.XXXXXXXXXX`
-wget https://github.com/ONSdigital/design-system/releases/download/54.0.1/templates.zip -O $TMPFILE
+
+wget https://github.com/ONSdigital/design-system/releases/download/55.1.0/templates.zip -O $TMPFILE
 rm -rf sml_builder/templates/components
 rm -rf sml_builder/templates/layout
+
 unzip -d ./sml_builder $TMPFILE
 rm $TMPFILE
