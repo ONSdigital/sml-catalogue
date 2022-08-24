0bb0cb93b509dd6722c974ccbd1d4fbfae8e86b2
Ben Latham <benlatham123@hotmail.co.uk>
2022-08-24 15:23:43 +0100

    take out the bins

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-echo "install pipenv"
-pip install pipenv
-echo "Make virtual environment"
-python3 -m venv venv
-source venv/bin/activate
-echo "Pipenv sync"
-pipenv sync --dev
-echo "Install terraform"
-apt-get install terraform
