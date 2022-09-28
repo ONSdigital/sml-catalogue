9fa2b53be681e4937cd340da997c8c44bf960e0d
Ben Latham <benlatham123@hotmail.co.uk>
2022-09-28 18:04:29 +0100

    Delete file.py

--- file.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-import os
-
-assets = json.loads(sys.argv[1])
-for asset in assets:
-    if asset["name"] == "file.zip":
-        os.environ["ASSET_URL"] = asset["url"]
-        break
