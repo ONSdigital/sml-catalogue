5cf659d89637aabf88d1c88618047be8a9c7c246
jhmjam <james.morgan@ons.gov.uk>
2023-01-03 15:15:19 +0000

    Sanjeev added tests for the dropdown

--- features/steps/method.py
+++ features/steps/method.py
@@ -7,18 +7,36 @@ import time
 from behave import *
 driver = webdriver.Chrome()
 
-
 @given('I\'m an sml portal user trying to get to the methods catalogue page')
 def auth_user(context):
     driver.get('http://localhost:5000/')
-
 
 @when('I navigate to the methods catalogue page')
 def navigate_to_url(context):
     driver.find_element(By.ID, value='title1').click()
 
-
 @then('The title of the methods catalogue page is "{title}"')
 def check_title(context, title):
     page_title = driver.find_element(By.TAG_NAME, "h1").text
     assert page_title == title
+
+@given('I am on the methods catalogue page')
+def auth_user(context):
+    driver.get('http://localhost:5000/methods')
+
+@when('I click on the collapsible drop down')
+def navigate_to_url(context):
+    collapsible = driver.find_element(By.ID, value='collapsible')
+    collapsible.find_element(By.TAG_NAME, "summary").click()
+
+@then('I see the dropdown content "{text}"')
+def check_title(context, text):
+    content_div = driver.find_element(By.ID, value='collapsible-content')
+    dropdown_content_elements = content_div.find_elements(By.TAG_NAME, "p")
+    dropdown_content = ""
+    for i in range (len(dropdown_content_elements)):
+        dropdown_content += dropdown_content_elements[i].text
+        if i < len(dropdown_content_elements)-1:
+            dropdown_content += " "
+    print(dropdown_content)
+    assert dropdown_content == text
