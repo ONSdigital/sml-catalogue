bf6db9822f8ebe3bffc3e56f125f87ac18670c73
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-10 12:46:08 +0000

    Trying to get env variables

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import socket
+import os
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(options=options)
@@ -10,3 +11,5 @@ local_ip = socket.gethostbyname(hostname)
 
 local_ip = f"http://{local_ip}/"
 print(local_ip)
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
