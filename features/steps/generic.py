c072bd93b8b97a9d3fcc2b5bdcd9452ec7216b4a
jhmjam <james.morgan@ons.gov.uk>
2023-01-12 14:56:07 +0000

    added print statements

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -21,4 +21,7 @@ def navigate_to_url(context, page):
 @then('The title of the page is "{title}"')
 def check_title(context, title):
     page_title = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
+    print(title)
+    print(page_title)
     assert page_title == title
+
