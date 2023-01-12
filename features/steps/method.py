cb11197d836761ed3445cdff0bf2b0b3184baa64
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-12 11:53:43 +0000

    Method updated & CI updated

--- features/steps/method.py
+++ features/steps/method.py
@@ -1,10 +1,9 @@
 # Step definitions for methods catalogue page
-
+from urllib.parse import urljoin
 import setupSelenium
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from behave import *
-import time
-
 driver = setupSelenium.driver
 host = setupSelenium.website_url
 
@@ -16,35 +15,32 @@ def auth_user(context):
 
 @when('I navigate to the methods catalogue page')
 def navigate_to_url(context):
-    driver.find_element(By.ID, value='title1').click()
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value='title1')).click()
 
 
 @then('The title of the methods catalogue page is "{title}"')
 def check_title(context, title):
-    page_title = driver.find_element(By.TAG_NAME, "h1").text
+    page_title = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
     assert page_title == title
 
 
 @given('I am on the methods catalogue page')
 def auth_user(context):
-    driver.get(host + "/methods/")
+    driver.get(f"{host}methods")
 
 
 @when('I click on the collapsible drop down')
 def navigate_to_url(context):
-    collapsible = driver.find_element(By.ID, value='collapsible')
-    collapsible.find_element(By.TAG_NAME, "summary").click()
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "summary")).click()
 
 
 @then('I see the dropdown content "{text}"')
 def check_title(context, text):
-    content_div = driver.find_element(By.ID, value='collapsible-content')
+    content_div = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value='collapsible-content'))
     dropdown_content_elements = content_div.find_elements(By.TAG_NAME, "p")
     dropdown_content = ""
-
-    for i in range (len(dropdown_content_elements)):
+    for i in range(len(dropdown_content_elements)):
         dropdown_content += dropdown_content_elements[i].text
         dropdown_content += " "
-
     dropdown_content = dropdown_content.rstrip()
     assert dropdown_content == text
