713ae12101326e2aab0b4f0388aeb177988af2d3
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-12 15:11:47 +0000

    Testing with generics

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -1,25 +1,25 @@
 # Step definitions for about page
 
-# from urllib.parse import urljoin
-# import setupSelenium
-# from selenium.webdriver.common.by import By
-# from behave import *
+from urllib.parse import urljoin
+import setupSelenium
+from selenium.webdriver.common.by import By
+from behave import *
 
-# driver = setupSelenium.driver
-# host = setupSelenium.website_url
-
-
-# @given('I\'m an sml portal user')
-# def auth_user(context):
-#     driver.get(host)
+driver = setupSelenium.driver
+host = setupSelenium.website_url
 
 
-# @when('I navigate to the "{page}" page')
-# def navigate_to_url(context, page):
-#     driver.get(urljoin(host + "/resources/", page))
+@given('I\'m an sml portal user')
+def auth_user(context):
+    driver.get(host)
 
 
-# @then('The title of the page is "{title}"')
-# def check_title(context, title):
-#     page_title = driver.find_element(By.TAG_NAME, "h1").text
-#     assert page_title == title
+@when('I navigate to the "{page}" page')
+def navigate_to_url(context, page):
+    driver.get(urljoin(host + "resources/", page))
+
+
+@then('The title of the page is "{title}"')
+def check_title(context, title):
+    page_title = driver.find_element(By.TAG_NAME, "h1").text
+    assert page_title == title
