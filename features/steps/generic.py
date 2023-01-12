a970de0dc1cdf4b23fe166d0d874386709231d94
jasonbellONS <jason.bell@ons.gov.uk>
2023-01-12 17:10:28 +0000

    amending timeouts
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -15,15 +15,12 @@ def auth_user(context):
 
 @when('I navigate to the "{page}" page')
 def navigate_to_url(context, page):
-    print('host: ', host)
     driver.get(urljoin(host + "resources/", page))
-    print(urljoin(host + "resources/", page))
-    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, 'main-content')))
+    WebDriverWait(driver, timeout=30).until(EC.presence_of_element_located({id: 'main-content'}))
 
 @then('The title of the page is "{title}"')
 def check_title(context, title):
     page_title = driver.find_element({id: 'h1'})
-    print(driver.current_url)
     print(title)
     print(page_title)
     assert page_title == title
