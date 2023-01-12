d897e45945c8e3672b3712188342c6e4bb32fa63
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-12 12:37:22 +0000

    Debugging the home page tests

--- features/steps/date_adjustment.py
+++ features/steps/date_adjustment.py
@@ -11,10 +11,13 @@ host = setupSelenium.website_url
 @given('I\'m an sml portal user trying to get to the date adjustment method')
 def auth_user(context):
     driver.get(host)
+    print("Host: ", host)
 
 
 @when('I navigate to the date adjustment page')
 def navigate_to_date_adjustment_method(context):
+    test = driver.find_element(By.TAG_NAME, "h1").text
+    print(test)
     driver.find_element(By.ID, value='title1').click()
     driver.find_element(By.LINK_TEXT, value='Date adjustment').click()
 
