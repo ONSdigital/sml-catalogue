9db03bc9ba61b9a9260db419e70b31896e2992ff
jhmjam <james.morgan@ons.gov.uk>
2023-01-12 14:46:45 +0000

    corrected typo

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(options=options)
-if os.getenv('deploy-url'):
-    website_url = f"{os.getenv('deploy-url')}"
+if os.getenv('deploy_url'):
+    website_url = f"{os.getenv('deploy_url')}"
 else:
     website_url = "http://localhost:8000/"
