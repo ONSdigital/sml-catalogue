d56d0684dfc5037ec9742c871ed0768326a09240
jhmjam <james.morgan@ons.gov.uk>
2023-01-12 16:28:44 +0000

    quote type changed

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -11,10 +11,10 @@ host = setupSelenium.website_url
 
 @given('I\'m an sml portal user')
 def auth_user(context):
-    driver.get('www.google.com')
+    driver.get("www.google.com")
     page_title = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "gbqfbb")).text
     print(page_title)
-    assert page_title == 'I\'m Feeling Lucky'
+    assert page_title == "I\'m Feeling Lucky"
 
 @when('I navigate to the "{page}" page')
 def navigate_to_url(context, page):
