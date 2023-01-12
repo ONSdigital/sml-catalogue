34ba05ab326b6519d27f6234cbee6a74173646c8
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-12 16:57:05 +0000

    Refactored setupSelenium Python file.

--- features/steps/date_adjustment.py
+++ features/steps/date_adjustment.py
@@ -16,11 +16,13 @@ def auth_user(context):
 
 @when('I navigate to the date adjustment page')
 def navigate_to_date_adjustment_method(context):
+    print("Current URL 1: ", driver.current_url)
     test = driver.find_element(By.ID, "main-content")
+    print("Current URL 2: ", driver.current_url)
     test_text = test.find_element(By.TAG_NAME, "p").text
     print("Host: ", host)
     print("Test: ", test_text)
-    print("Current URL: ", driver.current_url)
+    print("Current URL 3: ", driver.current_url)
     driver.find_element(By.ID, value='title1').click()
     driver.find_element(By.LINK_TEXT, value='Date adjustment').click()
 
