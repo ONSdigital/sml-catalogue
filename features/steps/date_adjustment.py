cd2ae904848afe2ab8515ce66004716d067103f3
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-01-13 11:17:04 +0000

    Testing hardcoded url

--- features/steps/date_adjustment.py
+++ features/steps/date_adjustment.py
@@ -1,17 +1,17 @@
 # Tests for the date adjustment link
 
-from setupSelenium import returnDriver, returnWebURL
+from setupSelenium import returnDriver
 from selenium.webdriver.common.by import By
 from behave import *
 
 driver = returnDriver()
-host = returnWebURL()
+# host = returnWebURL()
 
 
 @given('I\'m an sml portal user trying to get to the date adjustment method')
 def auth_user(context):
-    driver.get(host)
-    print("Host: ", host)
+    driver.get("https://d1jgbw8ee9pybj.cloudfront.net/")
+    # print("Host: ", host)
 
 
 @when('I navigate to the date adjustment page')
@@ -20,7 +20,7 @@ def navigate_to_date_adjustment_method(context):
     test = driver.find_element(By.ID, "main-content")
     print("Current URL 2: ", driver.current_url)
     test_text = test.find_element(By.TAG_NAME, "p").text
-    print("Host: ", host)
+    # print("Host: ", host)
     print("Test: ", test_text)
     print("Current URL 3: ", driver.current_url)
     driver.find_element(By.ID, value='title1').click()
