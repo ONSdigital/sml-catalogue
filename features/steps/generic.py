8115c904ec2b9d43ca114e5d50cb1da9e48c33ac
jhmjam <james.morgan@ons.gov.uk>
2023-01-16 11:19:39 +0000

    removed code around remote bdd automated tests

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -7,28 +7,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from behave import *
 
 driver = setupSelenium.driver
-host = "https://d1jgbw8ee9pybj.cloudfront.net/"
+host = setupSelenium.website_url
 
 @given('I\'m an sml portal user')
 def auth_user(context):
     driver.get(host)
-    page_title = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element(By.ID, "title1")).text
-    print('Discover methods used by the Office for National Statistics')
-    print('page_title', page_title)
-    print('host:', driver.current_url)
+    page_title = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
     assert page_title == 'Discover methods used by the Office for National Statistics'
 
 @when('I navigate to the "{page}" page')
 def navigate_to_url(context, page):
-    print('host: ', host)
     driver.get(urljoin(host + "resources/", page))
-    print(urljoin(host + "resources/", page))
     WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, 'main-content')))
 
 @then('The title of the page is "{title}"')
 def check_title(context, title):
     page_title = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
-    print(title)
-    print(page_title)
     assert page_title == title
 
