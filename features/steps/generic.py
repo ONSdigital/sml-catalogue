8ad3da261b43b1eae0e02fa50eff43a2fa4499dc
jhmjam <james.morgan@ons.gov.uk>
2023-01-16 08:54:43 +0000

    added test for anthony

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -7,14 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from behave import *
 
 driver = setupSelenium.driver
-host = setupSelenium.website_url
+host = "https://d1jgbw8ee9pybj.cloudfront.net"
 
 @given('I\'m an sml portal user')
 def auth_user(context):
     driver.get(host)
     page_title = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
     print('Discover methods used by the Office for National Statistics')
-    print(page_title)
+    print('page_title', page_title)
+    print('host:', host)
     assert page_title == 'Discover methods used by the Office for National Statistics'
 
 @when('I navigate to the "{page}" page')
