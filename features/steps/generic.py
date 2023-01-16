6625eeada9322c254895a8840c3484e105d17eac
jhmjam <james.morgan@ons.gov.uk>
2023-01-16 09:14:22 +0000

    added test for anthony

--- features/steps/generic.py
+++ features/steps/generic.py
@@ -2,7 +2,7 @@
 from urllib.parse import urljoin
 import setupSelenium
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from behave import *
 
