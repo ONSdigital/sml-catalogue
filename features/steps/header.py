ee5ae1b398511dca1b6e89e24a1c9f78882e2390
jhmjam <james.morgan@ons.gov.uk>
2023-01-26 15:58:12 +0000

    SPP-8439-creat-glossary-and-about-tab

--- features/steps/header.py
+++ features/steps/header.py
@@ -23,6 +23,14 @@ def navigate_to_url(context, page):
         main_navigation =  WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value='main-navigation'))
         main_navigation.find_element(By.LINK_TEXT, value='Help centre').click()
         WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, 'main-content')))
+    elif page == "about this library":
+        main_navigation =  WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value='main-navigation'))
+        main_navigation.find_element(By.LINK_TEXT, value='About this library').click()
+        WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, 'main-content')))
+    elif page == "glossary":
+        main_navigation =  WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value='main-navigation'))
+        main_navigation.find_element(By.LINK_TEXT, value='Glossary').click()
+        WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, 'main-content')))
 
 
 @then('The title of this page is "{title}"')
