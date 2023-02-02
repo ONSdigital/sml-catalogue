5034e8b0c74eec0419313e60a3890ae05e8d202e
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-02-02 12:13:59 +0000

    Accessibility statement added

--- sml_builder/page.py
+++ sml_builder/page.py
@@ -29,7 +29,16 @@ def cookies_page():
 
 @app.route("/accessibility-statement")
 def accessibility_page():
-    return render_template("accessibility_statement.html")
+    try:
+        with open(
+            "./content/accessibility/accessibility-statement.md", "r", encoding="utf-8"
+        ) as input_file:
+            text = input_file.read()
+            escaped_text = escape(text)
+            body = Markup(markdown.markdown(escaped_text))
+    except OSError as e:
+        _page_not_found(e)
+    return render_template("accessibility_statement.html", page_body=body)
 
 
 @app.route("/.well-known/security.txt")
