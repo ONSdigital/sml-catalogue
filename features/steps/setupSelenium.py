cffcde10e46e4501e564ec923cf3afb6fd4889d0
jasonbellONS <jason.bell@ons.gov.uk>
2023-02-07 11:59:44 +0000

    reverting setupSelenium.py
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -3,5 +3,5 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(options=options)
-website_url = "http://127.0.0.1:5000/"
+website_url = "http://localhost:8000/"
 timeout = 5
