ee67c20baffcb7b50a56863d5fc00648188bd3c3
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-04-12 11:14:40 +0100

    Adding a hash

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -108,7 +108,7 @@ resource "aws_cloudfront_response_headers_policy" "noindex" {
     }
     content_security_policy {
       override                = true
-      content_security_policy = "script-src 'self'"
+      content_security_policy = "script-src 'sha256-AN/TTkrWvH4W7oCH0w79aph67vAli7rskVQ+0LFPnD4='"
     }
     referrer_policy {
       override        = true
