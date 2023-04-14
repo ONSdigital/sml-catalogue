a9bb9eb4f55d0c577a71ca70959636883d7707ed
Sanjeev Srithevan <41544303+sanjeevz3009@users.noreply.github.com>
2023-04-14 13:28:25 +0100

    Sorting Functionality Issue (#153)
    * Refactored sml.js file & bug fixes.
    
    Co-authored-by: jhmjam <james.morgan@ons.gov.uk>

---
+++ sml_builder/static/js/glossary.js
@@ -0,0 +1,17 @@
+// Back to Top
+
+//Get the button
+const mybutton = document.getElementById("topBtn");
+// document.querySelector.footer
+const footer = document.footer
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
