ae1fb16c396674a34a440e71ea0b956fb53d2fd9
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-04-14 13:13:58 +0100

    Refactored the sml.js script

--- sml_builder/static/js/methods_catalogue.js
+++ sml_builder/static/js/methods_catalogue.js
@@ -1,20 +1,3 @@
-// Back to Top
-
-//Get the button
-var mybutton = document.getElementById("topBtn");
-// document.querySelector.footer
-var footer = document.footer
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 /**
 Currently we have a problem with the ONS table macro where
