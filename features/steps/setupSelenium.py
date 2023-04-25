0e96e201e8aec97a2423b45e7414455f50e8ed5e
jasonbellONS <jason.bell@ons.gov.uk>
2023-04-25 14:16:29 +0100

    testing behave with prod environment
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
 host = "https://statisticalmethodslibrary.ons.gov.uk"
 timeout = 5
