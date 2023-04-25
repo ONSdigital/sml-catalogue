eb5ff5f32846459b505864cec0519ac8d7f7a2b4
jasonbellONS <jason.bell@ons.gov.uk>
2023-04-25 14:12:43 +0100

    testing behave with prod environment
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from axe_selenium_python import Axe
-from behave import *
+
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(options=options)
-host = "http://localhost:8000/"
+host = "https://statisticalmethodslibrary.ons.gov.uk"
 timeout = 5
