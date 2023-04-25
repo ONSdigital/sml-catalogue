c0f0a9f054bda421e6660aee1c9b3ea7826b7d6f
jasonbellONS <jason.bell@ons.gov.uk>
2023-04-25 14:58:09 +0100

    testing behave with prod environment
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from axe_selenium_python import Axe
+from behave import *
 
 options = Options()
 options.headless = True
