63f5c6c9fc1b30f0f62c9944cde8be3fb0f0badf
jhmjam <james.morgan@ons.gov.uk>
2023-04-26 10:28:34 +0100

    added new environments to setupselenium

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -13,6 +13,8 @@ options.headless = True
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=options)
-host = "https://statisticalmethodslibrary.ons.gov.uk/"
+# host = "https://statisticalmethodslibrary.ons.gov.uk/" # prod
+# host = "https://preprod-sml.aws.onsdigital.uk/" # preprod
+host = "https://d1jgbw8ee9pybj.cloudfront.net/" # dev
 # host = "http://localhost:8000/"
 timeout = 5
