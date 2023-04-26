3f4b25446893edaa4bc87b04e5aacfb42d144a9b
jhmjam <james.morgan@ons.gov.uk>
2023-04-26 08:04:52 +0100

    setup selenium with local host

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -6,10 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from axe_selenium_python import Axe
 from behave import *
 
+# For local testing use the host local host
+
 options = Options()
 options.headless = True
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=options)
 host = "https://statisticalmethodslibrary.ons.gov.uk/"
+# host = "http://localhost:8000/"
 timeout = 5
