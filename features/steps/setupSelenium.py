fb3d569bb2187e44af0625a103589e1d024e0ad4
jhmjam <james.morgan@ons.gov.uk>
2023-05-03 16:51:31 +0100

    revert

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -3,7 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from axe_selenium_python import Axe
 from behave import *
+import socket
+import os
 
 options = Options()
 options.headless = True
