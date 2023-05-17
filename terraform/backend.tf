52074f82fe2e37034a5c0a6e7bab78745947d281
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 10:27:39 +0100

    test with static dev account name
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@
 terraform {
   backend "s3" {
     region   = "eu-west-2"
-    role_arn = "arn:aws:iam::289259348294:role/spp-concourse-access-prod"
+    role_arn = "arn:aws:iam::115311790871:role/spp-concourse-sml-deployment-dev"
   }
 }
