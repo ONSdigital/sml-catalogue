46597a598c3d4f44bcf1940f910f3ab4e62f34bf
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 13:57:57 +0100

    adding build step to pipeline
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

---
+++ ci/tasks/build_files.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+
+# Runs static code testing and builds files
+
+set -euo pipefail
+
+
+pip install pipenv
+python3 -m venv venv
+source venv/bin/activate
+pipenv sync --dev
+black --check --diff sml_builder
+pylint sml_builder
+flake8 sml_builder
+bandit -r sml_builder
+echo "Installing the ONS design system"
+./get_design_system.sh
+echo "Freezing flask"
+python freeze.py
+zip -r build.zip build
