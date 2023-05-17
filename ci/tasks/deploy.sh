7702f37e261e108d7c7f4685639d8cbc114d8303
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 15:39:25 +0100

    amending deploy
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -9,5 +9,5 @@ set -euo pipefail
 workspace_name=`cat ./.git/resource/head_name| tr "[:upper:]" "[:lower:]"`
 echo Workspace: ${workspace_name}
 
-aws s3 sync build s3://sml-portal-$environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
-aws s3 sync build s3://sml-portal-$environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
+aws s3 sync build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
+aws s3 sync build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
