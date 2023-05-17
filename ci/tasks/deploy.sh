3796363b919a8d9c327e335a13c4c1285d89574e
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 14:29:20 +0100

    adding deploy step to pipeline
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

---
+++ ci/tasks/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Deploys build code to the s3 bucket
+
+set -euo pipefail
+
+: ${TF_VAR_environment}
+
+workspace_name=`cat ./.git/resource/head_name| tr "[:upper:]" "[:lower:]"`
+echo Workspace: ${TF_WORKSPACE}
+
+unzip build.zip
+aws s3 sync build s3://sml-portal-$environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
+aws s3 sync build s3://sml-portal-$environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
