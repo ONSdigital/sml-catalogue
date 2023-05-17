2a7d2cdb8fab429d424f77bdcaeb1d9c73ef9e79
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 14:52:26 +0100

    amending build script
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/build_files.sh
+++ ci/tasks/build_files.sh
@@ -9,10 +9,11 @@ pip install pipenv
 python3 -m venv venv
 source venv/bin/activate
 pipenv sync --dev
-pipenv run black --check --diff sml_builder
-pipenv run pylint sml_builder
-pipenv run flake8 sml_builder
-pipenv run bandit -r sml_builder
+pip install wrapt
+black --check --diff sml_builder
+pylint sml_builder
+flake8 sml_builder
+bandit -r sml_builder
 echo "Installing the ONS design system"
 ./get_design_system.sh
 echo "Freezing flask"
