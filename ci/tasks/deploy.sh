7c50520e160f317ea62600e24d787f2136570a08
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-17 15:56:09 +0100

    amending deploy
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -6,8 +6,9 @@ set -euo pipefail
 
 : ${TF_VAR_environment}
 
+
 workspace_name=`cat ./.git/resource/head_name| tr "[:upper:]" "[:lower:]"`
 echo Workspace: ${workspace_name}
-
-aws s3 sync build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
-aws s3 sync build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
+cd ./terraform
+aws s3 sync ../build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
+aws s3 sync ../build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
