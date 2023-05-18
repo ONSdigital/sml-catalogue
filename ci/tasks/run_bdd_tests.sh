f764ceeeedf818bc421b49bb1f3008d3d4778e5e
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-18 12:13:09 +0100

    amending bdd test scrips
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/run_bdd_tests.sh
+++ ci/tasks/run_bdd_tests.sh
@@ -2,6 +2,7 @@
 
 # Runs BDD testing on the deployed workspace
 
+: ${URL}
 
 cd repo
 pip install pipenv
@@ -9,7 +10,7 @@ pipenv install --dev
 date=$(date '+%Y_%m_%d')
 echo $date
 mkdir -p ./features/test_reports/dev/$date
-pipenv run behave -D host=$ENDPOINT --junit --junit-directory ./features/test_reports/dev/$date
+pipenv run behave -D host=$URL --junit --junit-directory ./features/test_reports/dev/$date
 cd ./features/test_reports/dev/$date
 ls
 cat TESTS-about.xml TESTS-accessibility.xml TESTS-cookies.xml TESTS-footer.xml TESTS-glossary.xml TESTS-header.xml TESTS-help_center.xml TESTS-method_summary.xml TESTS-methods.xml > test_report.xml
