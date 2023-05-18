a5fd8364de90d0f6c59145479b66ed24596c242a
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-18 09:31:52 +0100

    amending deploy
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -9,6 +9,13 @@ set -euo pipefail
 
 workspace_name=`cat ./.git/resource/head_name| tr "[:upper:]" "[:lower:]"`
 echo Workspace: ${workspace_name}
-cd ./terraform
-aws s3 sync ../build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
-aws s3 sync ../build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
+aws sts assume-role --output text \
+        --role-arn "${SHARED_ACCOUNT_ROLE}" \
+        --role-session-name concourse-pipeline-run  \
+        --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+        | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
+    export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
+    export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
+    export AWS_SESSION_TOKEN="$(cat SessionToken)"
+aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
+aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
