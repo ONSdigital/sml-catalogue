e3fd97e79ba55f6f235b8897925a02a4c70bdfd6
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-18 09:36:58 +0100

    amending deploy
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -14,8 +14,8 @@ aws sts assume-role --output text \
         --role-session-name concourse-pipeline-run  \
         --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
         | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
-    export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
-    export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
-    export AWS_SESSION_TOKEN="$(cat SessionToken)"
+export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
+export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
+export AWS_SESSION_TOKEN="$(cat SessionToken)"
 aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
 aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
