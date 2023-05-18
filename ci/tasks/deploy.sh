a6429170503b61a73254f61b79b93e3825fd5a70
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-18 10:07:02 +0100

    amending syntax
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -5,18 +5,17 @@
 set -euo pipefail
 
 : ${TF_VAR_environment}
-: ${SHARED_ACCOUNT_ROLE}
+: ${AWS_SERVICE_ROLE}
 
 workspace_name=`cat ./.git/resource/head_name| tr "[:upper:]" "[:lower:]"`
 echo Workspace: ${workspace_name}
 aws sts assume-role --output text \
-  --role-arn "${SHARED_ACCOUNT_ROLE}" \
+  --role-arn "${AWS_SERVICE_ROLE}" \
   --role-session-name concourse-pipeline-run  \
   --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
   | awk -F '\t' '{print $1 > ("AccessKeyId")}{print $2 > ("SecretAccessKey")}{print $3 > ("SessionToken")}'
 export AWS_ACCESS_KEY_ID="$(cat AccessKeyId)"
 export AWS_SECRET_ACCESS_KEY="$(cat SecretAccessKey)"
 export AWS_SESSION_TOKEN="$(cat SessionToken)"
-echo $(aws s3 ls)
 aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --content-type "text/html" --exclude "*.css" --exclude "*.js"
 aws s3 sync ./build s3://sml-portal-$TF_VAR_environment-$workspace_name --delete --exclude "*" --include "*.css" --include "*.js"
