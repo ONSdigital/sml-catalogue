b56f1964b6ddba49c3bf10c8de292374409fd2aa
jasonbellONS <jason.bell@ons.gov.uk>
2023-05-22 13:01:50 +0100

    Destroy script added,live pipeline updated, dev pipeline updated with live data
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

---
+++ ci/tasks/destroy_terraform.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -eu
+
+export HOME=$(pwd)
+
+echo "machine github.com login ${CONCOURSE_ACCESS_TOKEN} password x-oauth-basic" > ~/.netrc
+chmod 600 ~/.netrc
+
+cd repo
+
+# --------------------------
+
+export TF_VAR_head_sha=$(cat .git/resource/head_sha)
+export TF_VAR_head_sha_short=$(cat .git/resource/head_sha_short)
+#
+#export TF_VAR_ses_cognito_email_arn="arn:aws:ses:eu-west-1:111111111111:identity/destroy@fake.ons.gov.uk"
+#export TF_VAR_ses_cognito_email_address="destroy@fake.ons.gov.uk"
+
+# --------------------------
+
+cd terraform/environment
+
+terraform init \
+-upgrade \
+-backend-config "bucket=${S3_NAME}" \
+-backend-config "key=${S3_KEY}" \
+-backend-config "workspace_key_prefix=${WORKSPACE_KEY_INFIX}"
+
+# --------------------------
+
+export TF_WORKSPACE=$(cat ~/repo/.workspace)
+echo "Workspace: ${TF_WORKSPACE}"
+
+# --------------------------
+
+terraform destroy -auto-approve
+
+export TF_WORKSPACE=""
+terraform workspace delete "$(cat ../../.workspace)" || ( echo "ERROR: Could not delete workspace!" && exit 1)
---
+++ ci/tasks/init_metadata.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -eu
+
+cd repo
+
+if [ -f .git/resource/metadata.json ]; then
+
+  #
+  # This is triggered if the metadata is being generated from a PR (type: pull-request)
+  #
+
+  cd .git/resource/
+
+  cut -c1-7 < head_sha > head_sha_short
+
+  PR="$(cat pr)"
+  HEAD_NAME="$(cat head_name)"
+
+  cd ../..
+  printf "%s\n" "Deriving workspace name from PR name"
+  printf "%s" "${PR}-${HEAD_NAME}" | sed 's/[^a-zA-Z0-9]/-/g' | tr '[:upper:]' '[:lower:]' | sed 's/cognito/cogneeto/g'| cut -c1-13 | sed 's/-$//' > .workspace
+
+else
+
+  #
+  # This is triggered if the metadata is being generated without a PR (type: git)
+  #
+
+  apk add --no-progress git
+
+  mkdir -p .git/resource/
+
+  git rev-parse HEAD > .git/resource/head_sha
+  cut -c1-7 < .git/resource/head_sha > .git/resource/head_sha_short
+
+  printf "%s\n" "Setting workspace name to 'main'"
+  printf "%s" "main" >.workspace
+
+fi
+
+printf "\nContents of head_sha :  %s\n\n " "$(cat .git/resource/head_sha)"
+printf "\nContents of head_sha_short :  %s\n\n " "$(cat .git/resource/head_sha_short)"
+printf "\nContents of .workspace:  %s\n\n " "$(cat .workspace)"
