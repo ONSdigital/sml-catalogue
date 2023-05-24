4036e9b27247127146c8483988ff5cade185c092
JasonBellONS <95040942+JasonBellONS@users.noreply.github.com>
2023-05-24 15:40:05 +0100

    Revert "Amending terraform to only use variable version of deploy role"

--- terraform/core.tf
+++ terraform/core.tf
@@ -56,7 +56,7 @@ terraform {
 
   backend "s3" {
     region   = "eu-west-2"
-    role_arn = var.deployment_role
+    role_arn = "arn:aws:iam::115311790871:role/spp-concourse-sml-deployment-dev"
   }
 }
 
