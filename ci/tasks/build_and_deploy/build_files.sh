c0a9fe253a14f3b026dfc1aa8c764a18e2b7a6f1
jasonbellONS <jason.bell@ons.gov.uk>
2023-07-18 09:00:52 +0100

    fix: amending build files to set git config for preprod and prod runs
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/build_and_deploy/build_files.sh
+++ ci/tasks/build_and_deploy/build_files.sh
@@ -25,17 +25,17 @@ run_linting(){
   ./get_design_system.sh
   echo "Freezing flask"
 }
-
+git config --global user.email "spp-shared-services@example.com"
+git config --global user.name "spp-shared-services"
 if [ "$BUILD_TYPE" -eq 0 ]; then
   run_linting
   python freeze.py
 elif [ "$BUILD_TYPE" -eq 1 ]; then
-  git config --global user.email "spp-shared-services@example.com"
-  git config --global user.name "spp-shared-services"
   git fetch
   run_linting
   semantic-release publish --prerelease
 else
+  git fetch
   run_linting
   semantic-release publish
 fi
