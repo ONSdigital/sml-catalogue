58123afa781fd7ebeef8e15de7405a192beede52
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-09-19 16:10:57 +0100

    Breaking behave tests

--- ci/tasks/testing/run_bdd_tests.sh
+++ ci/tasks/testing/run_bdd_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Runs BDD testing on the deployed workspace
 
@@ -13,11 +14,7 @@ date=$(date '+%Y_%m_%d')
 echo $date
 mkdir -p ./features/test_reports/dev/$date
 poetry run behave -D host=$URL --junit --junit-directory ./features/test_reports/dev/$date
-ret=$?
 cd ./features/test_reports/dev/$date
 ls
 cat TESTS-about.xml TESTS-accessibility.xml TESTS-cookies.xml TESTS-footer.xml TESTS-glossary.xml TESTS-header.xml TESTS-help_center.xml TESTS-method_summary.xml TESTS-methods.xml > test_report.xml
 cat test_report.xml
-if [ "$ret" -eq 1 ]; then
-   exit -1
-fi
