d5dcb2c8e6539163df10e0edae2a55fa79731aeb
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-09-19 15:20:19 +0100

    shell commands updated

--- ci/tasks/testing/run_bdd_tests.sh
+++ ci/tasks/testing/run_bdd_tests.sh
@@ -13,7 +13,11 @@ date=$(date '+%Y_%m_%d')
 echo $date
 mkdir -p ./features/test_reports/dev/$date
 poetry run behave -D host=$URL --junit --junit-directory ./features/test_reports/dev/$date
+ret=$?
 cd ./features/test_reports/dev/$date
 ls
 cat TESTS-about.xml TESTS-accessibility.xml TESTS-cookies.xml TESTS-footer.xml TESTS-glossary.xml TESTS-header.xml TESTS-help_center.xml TESTS-method_summary.xml TESTS-methods.xml > test_report.xml
 cat test_report.xml
+if $ret === 1
+then
+    exit -1
