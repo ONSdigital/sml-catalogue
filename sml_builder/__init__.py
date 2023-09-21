f75e0d82a9bf0611a7f31fc90ca7ba9a84b5a733
sanjeevz3009 <sanjeevz1717@gmail.com>
2023-09-21 12:10:38 +0100

    Flake8 config updated and comments added to __init__.py file

--- sml_builder/__init__.py
+++ sml_builder/__init__.py
@@ -10,11 +10,18 @@ app.jinja_env.lstrip_blocks = True
 app.config["FREEZER_DEFAULT_MIMETYPE"] = "text/html"
 app.config["FREEZER_DESTINATION"] = "../build"
 
-import sml_builder.glossary  # noqa: F401
-import sml_builder.help_centre  # noqa: F401
-import sml_builder.method  # noqa: F401
-import sml_builder.page  # noqa: F401
-import sml_builder.utils  # noqa: F401
+# E401 module import but unused
+# We have to import the modules below here and they get
+# used in other files later on so ignoring this E401 error
+# We import the utils module and do use
+# E402 module level import not at top of file
+# We import the python files/ flask routes into the __init__.py file and we can't
+# import them at the top of the file so ignoring them
+import sml_builder.glossary  # noqa: E402
+import sml_builder.help_centre  # noqa: E402
+import sml_builder.method  # noqa: E402
+import sml_builder.page  # noqa: E402
+import sml_builder.utils  # noqa: F401, E402
 
 
 @app.route("/")
