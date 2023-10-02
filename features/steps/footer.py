fc5574c477945f43deca39efbf10a25733458c85
Sanjeev Srithevan <41544303+sanjeevz3009@users.noreply.github.com>
2023-10-02 12:16:44 +0100

    SPP-9926 Add New Packages (#189)
    * Added isort and packages placed in the correct groups within pyproject.toml and run_py_tools shell file updated
    
    * Step definition files formatted using black & isort added to the project
    
    * Covering all python files for linting using flake8 and pylint
    
    * isort ran on all the python files in the repo
    
    * run_py_tools.sh commands updated
    
    * Poetry lock file updated
    
    * Black command updated
    
    * Bandit, pylint, flake8 reconfigured
    
    * Excluding features python files from security scanning as we are using assert for testing purposes
    
    * Linting errors corrected
    
    * Bandit command updated
    
    * Pylint changes complete
    
    * Excluding venv from bandit security scan
    
    * Code formatted
    
    * Scenario removed to reflect the removal of a step definition
    
    * Lock file updated
    
    * Lock file updated
    
    * Pushing to repo
    
    * Pushing to repo
    
    * shell commands updated
    
    * shell commands updated
    
    * shell commands updated
    
    * Breaking behave tests
    
    * Breaking behave tests
    
    * Lock file updated
    
    * Removed the testing I did regarding fixing false passes in the concourse pipelines
    
    * Code formatted
    
    * Flake8 config updated and comments added to __init__.py file
    
    * Code formatted and linting issues fixed
    
    * Linting errors fixed, flake8 config changed and comments added
    
    * Test fixed
    
    * Code formatted

--- features/steps/footer.py
+++ features/steps/footer.py
@@ -1,6 +1,7 @@
 # Tests for the date adjustment link
+# pylint: disable=import-error, undefined-variable, unused-argument, function-redefined
 
-from setupSelenium import *
+from setupSelenium import EC, By, WebDriverWait, driver, timeout
 
 
 @given('I\'m an sml portal user trying to get to the "{page}" page')
@@ -10,11 +11,19 @@ def auth_user(context, page):
 
 @when('I click the "{page}" page on the footer')
 def navigate_to_url(context, page):
-    WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.LINK_TEXT, value=page)).click()
-    WebDriverWait(driver, timeout=timeout).until(EC.presence_of_element_located((By.ID, 'main-content')))
+    WebDriverWait(driver, timeout=timeout).until(
+        lambda d: d.find_element(By.LINK_TEXT, value=page)
+    ).click()
+    WebDriverWait(driver, timeout=timeout).until(
+        EC.presence_of_element_located((By.ID, "main-content"))
+    )
 
 
 @then('The title of page is "{title}"')
 def check_title(context, title):
-    page_title = WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.TAG_NAME, "h1")).text
+    page_title = (
+        WebDriverWait(driver, timeout=timeout)
+        .until(lambda d: d.find_element(By.TAG_NAME, "h1"))
+        .text
+    )
     assert page_title == title
--- features/steps/help-centre.py
+++ features/steps/help-centre.py
@@ -1,48 +1,80 @@
 # Step definitions for help centre page
+# pylint: disable=import-error, undefined-variable, unused-argument, function-redefined
 
-from setupSelenium import *
+from setupSelenium import EC, By, WebDriverWait, driver, timeout
 
-@given('I\'m an sml portal user trying to get to the help centre')
+
+@given("I'm an sml portal user trying to get to the help centre")
 def auth_user(context):
     driver.get(context.config.userdata.get("host"))
+
 
 @given('I\'m an sml portal user on the "{page}" page')
 def auth_user(context, page):
     if page == "find and view methods":
-        driver.get(f'{context.config.userdata.get("host")}help-centre/access/view-methods')
+        driver.get(
+            f'{context.config.userdata.get("host")}help-centre/access/view-methods'
+        )
     elif page == "submit a method request":
-        driver.get(f'{context.config.userdata.get("host")}help-centre/information/methods-request')
+        driver.get(
+            f'{context.config.userdata.get("host")}help-centre/information/methods-request'
+        )
     elif page == "coding standards":
-        driver.get(f'{context.config.userdata.get("host")}help-centre/information/coding-standards')
+        driver.get(
+            f'{context.config.userdata.get("host")}help-centre/information/coding-standards'
+        )
 
-@when('I navigate to the help centre page')
+
+@when("I navigate to the help centre page")
 def navigate_to_url(context):
-    WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.LINK_TEXT, value='Help centre')).click()
-    WebDriverWait(driver, timeout=timeout).until(EC.presence_of_element_located((By.ID, 'main-content')))
+    WebDriverWait(driver, timeout=timeout).until(
+        lambda d: d.find_element(By.LINK_TEXT, value="Help centre")
+    ).click()
+    WebDriverWait(driver, timeout=timeout).until(
+        EC.presence_of_element_located((By.ID, "main-content"))
+    )
+
 
 @when('I click the "{link}" link')
 def navigate_to_url(context, link):
-    WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.PARTIAL_LINK_TEXT, value=link)).click()
+    WebDriverWait(driver, timeout=timeout).until(
+        lambda d: d.find_element(By.PARTIAL_LINK_TEXT, value=link)
+    ).click()
 
-@when('I click the external user dropdown')
+
+@when("I click the external user dropdown")
 def navigate_to_url(context):
-    WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.ID, value='collapsibleONSExternalUserId')).click()
-    WebDriverWait(driver, timeout=timeout).until(EC.presence_of_element_located((By.ID, 'collapsibleONSExternalUserId-content')))
+    WebDriverWait(driver, timeout=timeout).until(
+        lambda d: d.find_element(By.ID, value="collapsibleONSExternalUserId")
+    ).click()
+    WebDriverWait(driver, timeout=timeout).until(
+        EC.presence_of_element_located((By.ID, "collapsibleONSExternalUserId-content"))
+    )
+
 
 @then('The drop down content is "{text}"')
 def check_title(context, text):
-    content_div = WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.ID, value='collapsibleONSExternalUserId-content'))
+    content_div = WebDriverWait(driver, timeout=timeout).until(
+        lambda d: d.find_element(By.ID, value="collapsibleONSExternalUserId-content")
+    )
     dropdown_content_elements = content_div.find_elements(By.TAG_NAME, "p")
     dropdown_content = ""
 
-    for i in range(len(dropdown_content_elements)):
-        dropdown_content += dropdown_content_elements[i].text
+    for element in dropdown_content_elements:
+        dropdown_content += element.text
         dropdown_content += " "
 
-    dropdown_content = dropdown_content.replace('(opens in a new tab)', '').replace('\n', '').rstrip()
+    dropdown_content = (
+        dropdown_content.replace("(opens in a new tab)", "").replace("\n", "").rstrip()
+    )
     assert dropdown_content == text
+
 
 @then('The subtitle of the help centre page is "{subtitle}"')
 def check_title(context, subtitle):
-    page_title = WebDriverWait(driver, timeout=timeout).until(lambda d: d.find_element(By.LINK_TEXT, subtitle)).text
+    page_title = (
+        WebDriverWait(driver, timeout=timeout)
+        .until(lambda d: d.find_element(By.LINK_TEXT, subtitle))
+        .text
+    )
     assert page_title == subtitle
