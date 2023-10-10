35b786df2cd11982c6e80065779ad9c370838a8a
jasonbellONS <jason.bell@ons.gov.uk>
2023-10-10 08:29:32 +0100

    ci:fixing behave issues
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -16,5 +16,6 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--remote-debugging-port=9222")
+options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
 timeout = 5
