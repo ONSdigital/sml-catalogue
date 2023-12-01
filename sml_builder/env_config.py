7b2cd6da13f2500049014e3ced7aa656fe9d5efc
Steve Gibbard <steve.gibbard@ons.gov.uk>
2023-12-01 12:09:30 +0000

    Format changes for black

--- sml_builder/env_config.py
+++ sml_builder/env_config.py
@@ -1,10 +1,10 @@
 class EnvConfig:
     ENVIRONMENTS = {
-        'dev': 'GTM-DEV-ID',
-        'preprod': 'GTM-PREPROD-ID',
-        'prod': 'GTM-PROD-ID',
+        "dev": "GTM-DEV-ID",
+        "preprod": "GTM-PREPROD-ID",
+        "prod": "GTM-PROD-ID",
     }
 
     @staticmethod
     def get_environment_name(environment):
-        return EnvConfig.ENVIRONMENTS.get(environment, 'GTM-DEFAULT-ID')
+        return EnvConfig.ENVIRONMENTS.get(environment, "GTM-DEFAULT-ID")
