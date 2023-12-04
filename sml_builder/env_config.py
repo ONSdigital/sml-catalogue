413543849cb136edbeacd160626cec59da1fe98e
jhmjam <james.morgan@ons.gov.uk>
2023-12-04 09:07:56 +0000

    env-name now integrated

--- sml_builder/env_config.py
+++ sml_builder/env_config.py
@@ -1,8 +1,8 @@
 class EnvConfig:
     ENVIRONMENT_GA_CODE = {
-        "dev": "placeholder-dev-code",
-        "preprod": "placeholder-preprod-code",
-        "prod": "placeholder-prod-code",
+        "dev": "MBGteKWsXZpOgxp0C4pztA",
+        "preprod": "v9By2ndgSQ5Jx7abY30mxw",
+        "prod": "ETsGJBpGMS4CXqLVsXqy",
     }
 
     @staticmethod
