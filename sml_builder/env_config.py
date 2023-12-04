9262b1a060d81b47aa2610b610c90377cf31bf3d
jhmjam <james.morgan@ons.gov.uk>
2023-12-04 11:43:14 +0000

    test pr auth link

--- sml_builder/env_config.py
+++ sml_builder/env_config.py
@@ -1,6 +1,6 @@
 class EnvConfig:
     ENVIRONMENT_GA_CODE = {
-        "dev": "MBGteKWsXZpOgxp0C4pztA",
+        "dev": "i3OWrpouWsvLzw4VGgIbjA",
         "preprod": "v9By2ndgSQ5Jx7abY30mxw",
         "prod": "ETsGJBpGMS4CXqLVsXqy",
     }
