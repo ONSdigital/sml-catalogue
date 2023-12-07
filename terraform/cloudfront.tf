624322313767b87b1a972a19eadbfa6ba7ca9e9b
jhmjam <james.morgan@ons.gov.uk>
2023-12-07 14:30:55 +0000

    added hash key

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -108,7 +108,7 @@ resource "aws_cloudfront_response_headers_policy" "noindex" {
     }
     content_security_policy {
       override                = true
-      content_security_policy = "script-src 'self' 'sha256-AN/TTkrWvH4W7oCH0w79aph67vAli7rskVQ+0LFPnD4=' 'sha256-FM5wgoZgD+IUY/B3phTcQwLPjMdoyegYYa9Lfsx8a0Q=' 'sha256-U8ubqILKi55jm+DB8PdIpsaeSGlMw7ojOd+USMt3+2U=' 'sha256-VFSAQegS8mTZ872sLRVI+/L8kdl8Jc8qcGF3hlVR8lI=' 'sha256-my6PUM5Kgt3Lb0i849R7BdyF+JEnesypA2QrJRdq85w=' 'sha256-lPX6CW7ExjNsDgKbXUD9ZxHvlDQh4pYkIH8U1+TkXek=' 'sha256-nThNAb0aRMwXnzoWfgifomFEhBGUwiCDjEEmTbfwI6A=' 'sha256-zLKb3xjPU4JHY1uuSnjNKqhLK3ir4+/nzTHFMBumvWQ=' 'sha256-6+JP3/moiu9f6EvKT1WDZ+PJAVdPWV2UynUvzw2F3/A=' https://cdn.ons.gov.uk https://www.googletagmanager.com"
+      content_security_policy = "script-src 'self' 'sha256-AN/TTkrWvH4W7oCH0w79aph67vAli7rskVQ+0LFPnD4=' 'sha256-FM5wgoZgD+IUY/B3phTcQwLPjMdoyegYYa9Lfsx8a0Q=' 'sha256-U8ubqILKi55jm+DB8PdIpsaeSGlMw7ojOd+USMt3+2U=' 'sha256-VFSAQegS8mTZ872sLRVI+/L8kdl8Jc8qcGF3hlVR8lI=' 'sha256-my6PUM5Kgt3Lb0i849R7BdyF+JEnesypA2QrJRdq85w=' 'sha256-lPX6CW7ExjNsDgKbXUD9ZxHvlDQh4pYkIH8U1+TkXek=' 'sha256-nThNAb0aRMwXnzoWfgifomFEhBGUwiCDjEEmTbfwI6A=' 'sha256-zLKb3xjPU4JHY1uuSnjNKqhLK3ir4+/nzTHFMBumvWQ=' 'sha256-6+JP3/moiu9f6EvKT1WDZ+PJAVdPWV2UynUvzw2F3/A=' 'sha256-Au6k4E1teH1KFCB1htV6HiBhpaF4Pql/FtRpt1vRK9w=' https://cdn.ons.gov.uk https://www.googletagmanager.com"
     }
     referrer_policy {
       override        = true
