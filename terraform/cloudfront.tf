417a64b9e00bcc54cedd28d13122f196e84fb61c
jhmjam <james.morgan@ons.gov.uk>
2024-01-10 15:33:43 +0000

    added subscription

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -170,6 +170,12 @@ resource "aws_sns_topic" "sns_topic" {
   name     = "smlTopic"
 }
 
+resource "aws_sns_topic_subscription" "email-target" {
+  topic_arn = aws_sns_topic.sns_topic.arn
+  protocol  = "email"
+  endpoint  = "james.morgan@ons.gov.uk"
+}
+
 output "sns_topic_arn" {
  value = aws_sns_topic.sns_topic.arn
 }
