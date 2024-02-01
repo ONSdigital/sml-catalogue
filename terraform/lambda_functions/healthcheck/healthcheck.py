6d81990ddbce1e6f62e56691bb94e1e7c9a6fc57
jhmjam <james.morgan@ons.gov.uk>
2024-02-01 13:01:12 +0000

    ignore isort import error

--- terraform/lambda_functions/healthcheck/healthcheck.py
+++ terraform/lambda_functions/healthcheck/healthcheck.py
@@ -1,31 +1,32 @@
-import requests
+import requests # isort:skip
 
 # Lambda function to check if specified site has the main
 # title text and returns the expected status code. 
 def lambda_handler(event, context):
-    url = 'https://dev-sml.aws.onsdigital.uk/'
+    urls_to_check = ['https://dev-sml.aws.onsdigital.uk/', 'https://preprod-sml.aws.onsdigital.uk/', 'https://statisticalmethodslibrary.ons.gov.uk/']
     timeout = 5
     
-    try:
-        response = requests.get(url, timeout=timeout)
+    for url in urls_to_check:
+        try:
+            response = requests.get(url, timeout=timeout)
 
 
-        verify_text = 'An open source library for statistical code approved by the ONS'
-        if verify_text in response.text:
+            verify_text = 'An open source library for statistical code approved by the ONS'
+            if verify_text in response.text:
+                result = {
+                    'statusCode': response.status_code,
+                    'body': f'"{url}" is healthy'
+                }
+            else:
+                result = {
+                    'statusCode': response.status_code,
+                    'body': f'"{url}" is not healthy'
+                }
+
+        except requests.RequestException as e:
             result = {
-                'statusCode': response.status_code,
-                'body': f'"{url}" is healthy'
+                'statusCode': 500,
+                'body': f'Request error: {str(e)}'
             }
-        else:
-            result = {
-                'statusCode': response.status_code,
-                'body': f'"{url}" is not healthy'
-            }
-
-    except requests.RequestException as e:
-        result = {
-            'statusCode': 500,
-            'body': f'Request error: {str(e)}'
-        }
 
     return result
