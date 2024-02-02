f8801de64b564839dc25b82d968c1749f1e4b33c
jhmjam <james.morgan@ons.gov.uk>
2024-02-02 13:40:14 +0000

    corrected typo

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -158,7 +158,7 @@ resource "aws_lambda_function" "healthcheck" {
   memory_size   = 512
 
   environment {
-      site = "local.domain_name_base[var.environment]",
+      site = local.domain_name_base[var.environment],
   }
 
   tags = {
