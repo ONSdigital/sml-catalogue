aaf7fe861d64b98188e8eef27dfd1941b002bb4e
jhmjam <james.morgan@ons.gov.uk>
2024-02-06 13:11:13 +0000

    corrected region typo

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -236,7 +236,7 @@ module "route53" {
 resource "aws_route53_health_check" "sml" {
   type                            = "CLOUDWATCH_METRIC"
   cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.environment_health_check_alarm.alarm_name
-  cloudwatch_alarm_region         = aws.us_east_1
+  cloudwatch_alarm_region         = "us_east_1"
   insufficient_data_health_status = "Unhealthy"
 
   tags = {
