b5cc3901d6e9ec0af1a76aa5b58301a088d23333
jhmjam <james.morgan@ons.gov.uk>
2024-02-06 11:55:25 +0000

    changed health status for insufficent data

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -196,7 +196,7 @@ resource "aws_iam_role" "lambda_healthcheck" {
 }
 
 resource "aws_lambda_function" "healthcheck" {
-  provider      = aws.us-west-1
+  provider      = aws.us_west_1
   role          = aws_iam_role.lambda_healthcheck.arn
 
   function_name = "${var.environment}-healthcheck"
@@ -237,8 +237,8 @@ module "route53" {
 resource "aws_route53_health_check" "sml" {
   type                            = "CLOUDWATCH_METRIC"
   cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.environment_health_check_alarm.alarm_name
-  cloudwatch_alarm_region         = aws.us-west-1
-  insufficient_data_health_status = "Healthy"
+  cloudwatch_alarm_region         = aws.us_west_1
+  insufficient_data_health_status = "Unhealthy"
 
   tags = {
     Name = "${var.environment}_environment"
@@ -248,7 +248,7 @@ resource "aws_route53_health_check" "sml" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "environment_health_check_alarm" {
-  provider            = aws.us-west-1
+  provider            = aws.us_west_1
   alarm_name          = "${var.environment}_environment_alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
@@ -270,12 +270,12 @@ resource "aws_cloudwatch_metric_alarm" "environment_health_check_alarm" {
 }
 
 resource "aws_sns_topic" "sns_topic" {
-  provider = aws.us-west-1
+  provider = aws.us_west_1
   name     = "smlPortalTopic"
 }
 
 resource "aws_sns_topic_subscription" "email_target" {
-  provider = aws.us-west-1
+  provider = aws.us_west_1
   topic_arn = aws_sns_topic.sns_topic.arn
 
   protocol  = "email"
