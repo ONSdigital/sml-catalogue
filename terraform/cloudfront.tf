29e407e1c7661aefe4efce388f0ab1e37cb86f2f
jhmjam <james.morgan@ons.gov.uk>
2024-02-06 11:36:49 +0000

    changed health status for insufficent data

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -238,7 +238,7 @@ resource "aws_route53_health_check" "sml" {
   type                            = "CLOUDWATCH_METRIC"
   cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.environment_health_check_alarm.alarm_name
   cloudwatch_alarm_region         = aws.eu_west_2
-  insufficient_data_health_status = "Unhealthy"
+  insufficient_data_health_status = "Healthy"
 
   tags = {
     Name = "${var.environment}_environment"
