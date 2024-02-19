0d90c21ec0cde58baecc258b2c507c6822bb34f1
jhmjam <james.morgan@ons.gov.uk>
2024-02-19 15:32:59 +0000

    changed infrastructure of terraform code

--- terraform/dns/healthcheck/vars.tf
+++ terraform/dns/healthcheck/vars.tf
@@ -9,10 +9,6 @@ variable "domain_name_base" {
   type = string
 }
 
-output "cert_arn" {
-  value = aws_acm_certificate.sml.arn
-}
-
 output "website_url" {
   value = "https://${var.domain_name_base}/"
 }
