6e2b9aaf5b2fa072268e9a375b572e54d3dc80b0
jhmjam <james.morgan@ons.gov.uk>
2024-02-19 14:56:47 +0000

    changed infrastructure of terraform code

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -128,6 +128,11 @@ module "route53" {
     domain_name    = aws_cloudfront_distribution.sml-catalogue.domain_name
     hosted_zone_id = aws_cloudfront_distribution.sml-catalogue.hosted_zone_id
   }
+
+  environment = var.environment
+
+  deployment_role = var.deployment_role
+  
   domain_name_base = local.domain_name_base[var.environment]
 }
 
