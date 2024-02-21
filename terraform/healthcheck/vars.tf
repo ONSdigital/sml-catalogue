1412e58d7a5d5e66cca6102a9ba2df42cd5bda81
jhmjam <james.morgan@ons.gov.uk>
2024-02-21 13:53:20 +0000

    changed healthcheck module

--- terraform/healthcheck/vars.tf
+++ terraform/healthcheck/vars.tf
@@ -6,10 +6,6 @@ variable "deployment_role" {
   type = string
 }
 
-locals {
-  domain_name_base = {
-    dev : "dev-sml.aws.onsdigital.uk"
-    preprod : "preprod-sml.aws.onsdigital.uk"
-    prod : "statisticalmethodslibrary.ons.gov.uk"
-  }
+variable "domain_name_base" {
+  type = string
 }
