958001cacffa512acb73a512aeeafd7db2e52a28
jhmjam <james.morgan@ons.gov.uk>
2024-02-21 09:16:12 +0000

    moved files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,30 @@
 variable "environment" {
   type = string
 }
+
 variable "deployment_role" {
   type = string
 }
+
 locals {
   domain_name_base = {
     dev : "dev-sml.aws.onsdigital.uk"
     preprod : "preprod-sml.aws.onsdigital.uk"
     prod : "statisticalmethodslibrary.ons.gov.uk"
   }
+}
+
+variable "s3_bucket" {
+  type = object({
+    domain_name    = string
+    hosted_zone_id = string
+  })
+}
+
+variable "domain_name_base" {
+  type = string
+}
+
+output "website_url" {
+  value = "https://${var.domain_name_base}/"
 }
