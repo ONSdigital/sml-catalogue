3548955478968102cab2512eb3d4a06e9f449feb
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-02-23 15:50:57 +0000

    add json loader util function

--- sml_builder/utils.py
+++ sml_builder/utils.py
@@ -3,6 +3,7 @@ import re
 from flask import abort
 
 from sml_builder import app
+from json import load
 
 
 @app.template_filter("convert_name")
@@ -14,3 +15,12 @@ def convert_term(value):
 def _page_not_found(error):
     print(error)
     abort(404)
+
+def get_feature_config(feature_name: str):
+    with open("config/feature.json", "r", encoding="utf-8") as features_file:
+        features = load(features_file)["features"]
+
+    try:
+        return features[feature_name]
+    except KeyError:
+        raise KeyError(f"Feature '{feature_name}' not found in features.")
