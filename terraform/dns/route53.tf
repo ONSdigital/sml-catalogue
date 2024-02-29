be4f7c17d096c8162eccea51626713360ed8f532
jhmjam <james.morgan@ons.gov.uk>
2024-02-29 15:39:33 +0000

    moved route53

--- terraform/dns/route53.tf
+++ terraform/dns/route53.tf
@@ -46,17 +46,3 @@ resource "aws_route53_record" "cert-validations" {
   type            = each.value.type
   zone_id         = data.aws_route53_zone.sml.zone_id
 }
-
-# creates route53 and its dependency on cloudwatch alarms
-resource "aws_route53_health_check" "sml" {
-  type                            = "CLOUDWATCH_METRIC"
-  cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.healthcheck.alarm_name
-  cloudwatch_alarm_region         = "eu-west-2"
-  insufficient_data_health_status = "Healthy"
-
-  tags = {
-    Name = "${var.environment}_environment"
-  }
-
-  depends_on                      = [aws_cloudwatch_metric_alarm.healthcheck]
-}
