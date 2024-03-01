542d2bef76d87215c9dd934dcc17860b5d39186e
jhmjam <james.morgan@ons.gov.uk>
2024-03-01 10:45:38 +0000

    change output structure

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,11 +6,6 @@ output "cloudfront_id" {
   value = aws_cloudfront_distribution.sml-catalogue.id
 }
 
-output "cert_arn" {
-  source = "./dns"
-  value = aws_acm_certificate.sml.arn
-}
-
 output "website_url" {
   value = length(module.route53) > 0 ? module.route53[0].website_url : "https://${aws_cloudfront_distribution.sml-catalogue.domain_name}/"
 }
