ca890cab28a415bf914e3877e4137c9791f32ad4
jhmjam <james.morgan@ons.gov.uk>
2024-03-01 10:16:19 +0000

    remove duplicate

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,9 +6,10 @@ output "cloudfront_id" {
   value = aws_cloudfront_distribution.sml-catalogue.id
 }
 
-output "website_url" {
-  value = length(module.route53) > 0 ? module.route53[0].website_url : "https://${aws_cloudfront_distribution.sml-catalogue.domain_name}/"
-}
 output "cert_arn" {
   value = aws_acm_certificate.sml.arn
+}
+
+output "website_url" {
+  value = "https://${var.domain_name_base}/"
 }
