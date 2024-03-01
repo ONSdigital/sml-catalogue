a69c6cb4998247b792fa87140689ede88d161fd6
jhmjam <james.morgan@ons.gov.uk>
2024-03-01 11:23:44 +0000

    added roles to event for new format test

---
+++ terraform/events.tf
@@ -0,0 +1,56 @@
+# This rule triggers the healthcheck lambda every minute
+resource "aws_cloudwatch_event_rule" "trigger_healthcheck" {
+    name                = "${local.domain_name_base[var.environment]}-healthcheck-trigger"
+    description         = "Fires the healthcheck lambda function every minute"
+    schedule_expression = "rate(1 minute)"
+
+policy = jsonencode({
+    #tfsec:ignore:aws-iam-no-policy-wildcards
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+                    "events:PutRule",
+                    "events:DescribeRule",
+                    "events:DeleteRule",
+                    "events:ListRules",
+                    "events:ListRuleNamesByTarget",
+                    "events:ListTagsForResource",
+                    "events:PutTargets",
+                    "events:ListTargetsByRule",
+                    "events:RemoveTargets"
+                    ],
+        Resource = [
+            "arn:aws:events:eu-west-2:115311790871:rule/${local.domain_name_base[var.environment]}-healthcheck-trigger"
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["kms:Decrypt"]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
+
+# Points to the healthcheck lambda
+resource "aws_cloudwatch_event_target" "sml_site_trigger_healthcheck" {
+    rule      = "${aws_cloudwatch_event_rule.trigger_healthcheck.name}"
+    target_id = "check_sml_site"
+    arn       = "${aws_lambda_function.healthcheck.arn}"
+    input     = jsonencode({
+                  "site"            : "https://${local.domain_name_base[var.environment]}",
+                  "env"             : "${var.environment}",
+                  "expected_string" : "An open source library for statistical code approved by the ONS"
+                })
+}
+
+# Adds permission for event to invoke healthcheck function
+resource "aws_lambda_permission" "allow_event_to_invoke_healthcheck" {
+    statement_id  = "AlarmAction"
+    action        = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.healthcheck.function_name}"
+    principal     = "events.amazonaws.com"
+    source_arn    = "${aws_cloudwatch_event_rule.trigger_healthcheck.arn}"
+}
--- terraform/healthcheck_lambda.tf
+++ terraform/healthcheck_lambda.tf
@@ -1,31 +1,3 @@
-# This rule triggers the healthcheck lambda every minute
-resource "aws_cloudwatch_event_rule" "trigger_healthcheck" {
-    name                = "${local.domain_name_base[var.environment]}-healthcheck-trigger"
-    description         = "Fires the healthcheck lambda function every minute"
-    schedule_expression = "rate(1 minute)"
-}
-
-# Points to the healthcheck lambda
-resource "aws_cloudwatch_event_target" "sml_site_trigger_healthcheck" {
-    rule      = "${aws_cloudwatch_event_rule.trigger_healthcheck.name}"
-    target_id = "check_sml_site"
-    arn       = "${aws_lambda_function.healthcheck.arn}"
-    input     = jsonencode({
-                  "site"            : "https://${local.domain_name_base[var.environment]}",
-                  "env"             : "${var.environment}",
-                  "expected_string" : "An open source library for statistical code approved by the ONS"
-                })
-}
-
-# Adds permission for event to invoke healthcheck function
-resource "aws_lambda_permission" "allow_event_to_invoke_healthcheck" {
-    statement_id  = "AlarmAction"
-    action        = "lambda:InvokeFunction"
-    function_name = "${aws_lambda_function.healthcheck.function_name}"
-    principal     = "events.amazonaws.com"
-    source_arn    = "${aws_cloudwatch_event_rule.trigger_healthcheck.arn}"
-}
-
 # Creates iam role
 resource "aws_iam_role" "healthcheck" {
   name               = "${var.environment}-healthcheck"
