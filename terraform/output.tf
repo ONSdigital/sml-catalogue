5cb849f4934fa5981aa467bbb454951b86bf7e9f
jhmjam <james.morgan@ons.gov.uk>
2024-03-01 10:39:53 +0000

    define output source

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,9 +7,10 @@ output "cloudfront_id" {
 }
 
 output "cert_arn" {
+  source = "./dns"
   value = aws_acm_certificate.sml.arn
 }
 
 output "website_url" {
-  value = "https://${var.domain_name_base}/"
+  value = length(module.route53) > 0 ? module.route53[0].website_url : "https://${aws_cloudfront_distribution.sml-catalogue.domain_name}/"
 }
