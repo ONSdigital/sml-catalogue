036b9e53f92d3f4a64fbe5b8fdd6fb3834ee9ab4
jhmjam <james.morgan@ons.gov.uk>
2024-03-01 09:52:36 +0000

    created output.tf file

---
+++ terraform/output.tf
@@ -0,0 +1,18 @@
+output "cf_website_url" {
+  value = "https://${aws_cloudfront_distribution.sml-catalogue.domain_name}/"
+}
+
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.sml-catalogue.id
+}
+
+output "website_url" {
+  value = length(module.route53) > 0 ? module.route53[0].website_url : "https://${aws_cloudfront_distribution.sml-catalogue.domain_name}/"
+}
+output "cert_arn" {
+  value = aws_acm_certificate.sml.arn
+}
+
+output "website_url" {
+  value = "https://${var.domain_name_base}/"
+}
