cc2354b4b991fbce43ed24a1f4ecff535e7cb8f0
jhmjam <james.morgan@ons.gov.uk>
2024-03-06 12:52:57 +0000

    adjusted variables

--- terraform/healthcheck/variables.tf
+++ terraform/healthcheck/variables.tf
@@ -2,15 +2,11 @@ variable "domain_name_base" {
   type = string
 }
 
-variable "email" {
-  sml_admin = "SMLAdmin@ons.gov.uk"
-}
-
 variable "environment" {
   type = string
 }
 
 variable "input" {
-  "url" = "https://${var.domain_name_base[var.environment]}"
-  "text" = "An open source library for statistical code approved by the ONS"
+  url = "https://${var.domain_name_base[var.environment]}"
+  text = "An open source library for statistical code approved by the ONS"
 }
