add3ce53528b4c6e536c4d4e52a256ca45d475d3
jhmjam <james.morgan@ons.gov.uk>
2024-03-07 12:21:15 +0000

    corrected typo

--- terraform/alerter/variables.tf
+++ terraform/alerter/variables.tf
@@ -6,7 +6,7 @@ variable "environment" {
   type = string
 }
 
-variable "slack_secret" {
+variable "slack_url" {
   type = string
   default = "https://hooks.slack.com/triggers/E04RP3ZJ3QF/6739592211926"
 }
