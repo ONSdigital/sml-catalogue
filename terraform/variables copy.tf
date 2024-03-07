012518900d2a2d11fd7d46e717b21bd8994c1ae5
jhmjam <james.morgan@ons.gov.uk>
2024-03-07 14:33:11 +0000

    add missing variable origin

---
+++ terraform/variables copy.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type = string
+}
+
+variable "slack_alert_token" {
+  type = string
+}
+
+variable "deployment_role" {
+  type = string
+}
+
+locals {
+  domain_name_base = {
+    dev : "dev-sml.aws.onsdigital.uk"
+    preprod : "preprod-sml.aws.onsdigital.uk"
+    prod : "statisticalmethodslibrary.ons.gov.uk"
+  }
+}
