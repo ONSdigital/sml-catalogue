1bfac0ea6cc57334bc2f9884c8abea100be0d88f
jhmjam <james.morgan@ons.gov.uk>
2024-03-07 12:29:44 +0000

    corrected typo

--- terraform/alerter/output.tf
+++ terraform/alerter/output.tf
@@ -1,7 +1,3 @@
 output "alerter_lambda" {
   value = aws_lambda_function.alerter.arn
 }
-
-output "slack_secret" {
-  value = data.aws_secretsmanager_secret_version.current.secret_string
-}
--- terraform/alerter/variables.tf
+++ terraform/alerter/variables.tf
@@ -10,3 +10,8 @@ variable "slack_url" {
   type = string
   default = "https://hooks.slack.com/triggers/E04RP3ZJ3QF/6739592211926"
 }
+
+variable "slack_secret" {
+  type = string
+  default = data.aws_secretsmanager_secret_version.current.secret_string
+}
