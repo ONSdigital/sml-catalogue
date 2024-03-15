d6505893ff5fefe0b3ae6ab6065488d74518bb41
jhmjam <james.morgan@ons.gov.uk>
2024-03-15 16:22:34 +0000

    SPP-run-main-only-for-healthcheck

--- terraform/healthcheck.tf
+++ terraform/healthcheck.tf
@@ -5,6 +5,6 @@ module "healthcheck" {
 
   deployment_role = var.deployment_role
 
-  alerter = arn:aws:lambda:eu-west-2:var.aws_account_id:function:var.deployment_role-alerter-var.terraform.workspace
+  alerter = "arn:aws:lambda:eu-west-2:${var.aws_account_id}:function:${var.deployment_role}-alerter-${var.terraform.workspace}"
   
 }
