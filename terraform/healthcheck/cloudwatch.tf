63ca9581e3dd6c722ece9d9cfbb32c6e928961eb
jhmjam <james.morgan@ons.gov.uk>
2024-03-15 15:45:51 +0000

    SPP-run-main-only-for-healthcheck

--- terraform/healthcheck/cloudwatch.tf
+++ terraform/healthcheck/cloudwatch.tf
@@ -19,10 +19,7 @@ resource "aws_cloudwatch_metric_alarm" "healthcheck" {
   threshold           = 3
   alarm_description   = "Alarm for ${local.domain_name_base[var.environment]} has been triggered"
   actions_enabled     = "true"
-  alarm_actions       = [
-                          aws_sns_topic.sns_topic.arn,
-                          arn:aws:lambda:eu-west-2:var.aws_account_id:function:var.deployment_role-alerter-var.terraform.workspace
-                        ]
+  alarm_actions       = [aws_sns_topic.sns_topic.arn, "arn:aws:lambda:eu-west-2:${var.aws_account_id}:function:${var.deployment_role}-alerter-${var.terraform.workspace}"]
   treat_missing_data  = "notBreaching"
 
   dimensions = {
