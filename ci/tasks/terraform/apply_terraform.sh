5a2dc56e09f5c553dedff35eb9cc1a1079b5b574
jhmjam <james.morgan@ons.gov.uk>
2024-03-18 08:07:28 +0000

    test with normal account id

--- ci/tasks/terraform/apply_terraform.sh
+++ ci/tasks/terraform/apply_terraform.sh
@@ -21,6 +21,7 @@ rm -f $HOME/.netrc
 echo "default login $username password $password" >> "${HOME}/.netrc"
 echo "starting terraform init"
 cd ./terraform
+TF_VAR_AWS_ACCOUNT_ID = $AWS_ACCOUNT_ID
 terraform init \
     -upgrade \
     -backend-config "bucket=${S3_NAME}" \
@@ -37,7 +38,7 @@ terraform apply \
     -var="environment=${TF_VAR_environment}" \
     -var="slack_alert_token=${TF_VAR_slack_alert_token}" \
     -var="deployment_role=${TF_VAR_deployment_role}" \
-    -var="aws_account_id=${AWS_ACCOUNT_ID}"
+    -var="aws_account_id=${TF_VAR_AWS_ACCOUNT_ID}"
 rm plan.tfstate
 echo "done"
 echo "DEPLOY_URL=`terraform output -raw website_url`" > ../../GITHUB_OUTPUT/output.txt
