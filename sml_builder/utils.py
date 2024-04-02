1ff75b5df4d2bf08e598c14ca83f480be71e4282
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-04-02 15:56:16 +0100

    fix cyclical import

--- sml_builder/utils.py
+++ sml_builder/utils.py
@@ -1,16 +1,6 @@
-import re
 from json import load
 
 from flask import abort, url_for
-
-from sml_builder import app
-
-
-@app.template_filter("convert_name")
-def convert_term(value):
-    converted = re.sub("[^0-9a-z]+", "-", value.lower())
-    return converted
-
 
 def checkTypeList(data):
     return isinstance(data, list)
