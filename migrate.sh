f3f7cfdfa821a2000e41ddc804db3bd58f30bbf7
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-04-08 14:19:51 +0100

    update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,1 +1,7 @@
-contentful merge export --source-environment-id dev --target-environment-id preprod --management-token $CLI_KEY --space-id $SPACE_ID
+ 
+#  TODO: ensure the content models are the same in both environments
+#  TODO: create a backup to allow rollbacks
+#  TODO: add variables to migrate.sh to allow export to occur between both dev-preprod and preprod-prod
+
+contentful space export --management-token $CLI_KEY --export-dir ./contentful-export --environment-id dev --content-file dev-export.json
+contentful space import --management-token $CLI_KEY --environment-id preprod --content-file contentful-export/dev-export.json
