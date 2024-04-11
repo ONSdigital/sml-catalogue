0f463c0981d545853f903c0cb5168e07c6968dda
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-04-11 16:59:45 +0100

    testing rollback

--- contentful-data/migrations/dev-export.js
+++ contentful-data/migrations/dev-export.js
@@ -1,3 +1,19 @@
 function migrationFunction(migration, context) {
+    const test = migration.createContentType("test");
+    test
+        .displayField("test")
+        .name("test")
+        .description("")
+
+    const testTest = test.createField("test");
+    testTest
+        .name("test")
+        .type("Symbol")
+        .localized(false)
+        .required(false)
+        .validations([])
+        .disabled(false)
+        .omitted(false)
+    test.changeFieldControl("test", "builtin", "singleLine")
 }
 module.exports = migrationFunction;
