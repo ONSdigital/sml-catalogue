9a3375650d0bdc7ec77fe358da54ed08f20b1941
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-04-25 15:17:19 +0100

    undo selenium debug

--- features/steps/setupSelenium.py
+++ features/steps/setupSelenium.py
@@ -15,5 +15,5 @@ options.headless = True
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--headless")
-driver = webdriver.Chrome()
-timeout = 25
+driver = webdriver.Chrome(options=options)
+timeout = 5
