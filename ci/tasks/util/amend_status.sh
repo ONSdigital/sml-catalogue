26efc9e4d554c63c7a0a764e49206f08ebb275e0
jasonbellONS <jason.bell@ons.gov.uk>
2024-08-08 11:28:23 +0100

    amending variables
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/util/amend_status.sh
+++ ci/tasks/util/amend_status.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 id=$(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $token" \
+  -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/ONSdigital/sml-catalogue/deployments \
   -d '{"ref":"spp11763"}' | jq -r '.id')
