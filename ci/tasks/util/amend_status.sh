1f0b7f47e69a1a26f7bbe45bc45eb05e1cb263ca
jasonbellONS <jason.bell@ons.gov.uk>
2024-08-08 11:12:23 +0100

    adding cookiefile to run curl
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/util/amend_status.sh
+++ ci/tasks/util/amend_status.sh
@@ -8,10 +8,10 @@ set -euo pipefail
 : ${STATE}
 : ${TOKEN}
 
-curl --cookie cookies.txt --cookie-jar newcookies.txt -L \
+curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://github.com/ONSdigital/sml-catalogue/deployments/dev/statuses \
+  https://api.github.com/ONSdigital/sml-catalogue/deployments/dev/statuses \
   -d "{'environment':'$ENVIRONMENT','state':'$STATE'}"
