4d55a210952386e67f650644702cf56f55d8c56e
jasonbellONS <jason.bell@ons.gov.uk>
2024-08-08 10:52:35 +0100

    Initial testing with pr branch
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

---
+++ ci/tasks/util/amend_status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# grabs email of user that triggered contentful update for post steps
+
+set -euo pipefail
+
+: ${ENVIRONMENT}
+: ${STATE}
+: ${TOKEN}
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/OWNER/REPO/deployments/DEPLOYMENT_ID/statuses \
+  -d "{'environment':'$ENVIRONMENT','state':'$STATE'}"
