44bbc47f657ad0c58c30ad210f269c89aeed6aab
jasonbellONS <jason.bell@ons.gov.uk>
2024-08-08 10:55:18 +0100

    adding github url
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/util/amend_status.sh
+++ ci/tasks/util/amend_status.sh
@@ -13,5 +13,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/OWNER/REPO/deployments/DEPLOYMENT_ID/statuses \
+  https://github.com/ONSdigital/sml-catalogue/deployments/dev/statuses \
   -d "{'environment':'$ENVIRONMENT','state':'$STATE'}"
