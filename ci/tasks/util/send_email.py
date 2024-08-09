976935f55cbcf8253d3203507c004cdcb79c6ed3
jasonbellONS <jason.bell@ons.gov.uk>
2024-08-09 10:43:49 +0100

    Test with email
    Signed-off-by: jasonbellONS <jason.bell@ons.gov.uk>

--- ci/tasks/util/send_email.py
+++ ci/tasks/util/send_email.py
@@ -2,16 +2,18 @@ import os
 
 from notifications_python_client.notifications import NotificationsAPIClient
 
+templates = {
+    "pass": "94772cac-74b6-4b12-afca-11e525e3365a",
+    "fail": "45a2acad-7f3b-4993-aac5-2ed618c26787",
+    "in_progress": "fd5fb69b-ec9c-408b-b761-5ff11266f50b"
+}
 firstname = os.environ["FIRSTNAME"]
 surname = os.environ["SURNAME"]
 email = os.environ["EMAIL"]
 api_key = os.environ["GOVNOTIFYAPIKEY"]
-end_state = os.environ["STATUS"]
-template = (
-    "94772cac-74b6-4b12-afca-11e525e3365a"
-    if end_state == "pass"
-    else "45a2acad-7f3b-4993-aac5-2ed618c26787"
-)
+state = os.environ["STATUS"]
+template = templates[state]
+
 personalisation = {"name": firstname, "surname": surname}
 notifications_client = NotificationsAPIClient(api_key)
 response = notifications_client.send_email_notification(
