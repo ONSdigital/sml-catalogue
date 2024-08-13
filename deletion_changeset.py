094204d0a97b683ea2ed3a7852fab43f6cc49fcb
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-08-13 14:56:50 +0100

    create python script to generate deletion changescript for rollbacks

---
+++ deletion_changeset.py
@@ -0,0 +1,39 @@
+from json import load, dump
+import os
+import time
+import sys
+
+if __name__ == "__main__":
+    allowed_environments = ["dev", "preprod", "prod"]
+    if len(sys.argv) == 2:
+        target_environment = sys.argv[1]
+    else:
+        print("Usage: python deletion_changeset.py <target_environment>")
+        sys.exit(1)
+    if target_environment not in allowed_environments:
+        print("Invalid environment. Allowed environments are dev, preprod, prod.")
+        sys.exit(1)
+    with open(
+        "./contentful-data/migrations/deletion-changeset.json", encoding="utf-8"
+    ) as deletion_changeset_file:
+        deletion_changeset = load(deletion_changeset_file)
+    with open(
+        f"./contentful-data/content-exports/{target_environment}-export.json",
+        encoding="utf-8",
+    ) as target_file:
+        target_environment_data = load(target_file)
+    unix_timestamp = int(time.time() * 1000)
+    deletion_changeset["sys"]["createdAt"] = str(unix_timestamp)
+    deletion_changeset["sys"]["target"]["sys"]["id"] = target_environment
+    deletion_changeset["items"] = [
+        {
+            "changeType": "delete",
+            "entity": {
+                "sys": {"type": "Link", "linkType": "Entry", "id": item["sys"]["id"]}
+            },
+        }
+        for item in target_environment_data["entries"]
+        if item["sys"]["environment"]["sys"]["id"] == target_environment
+    ]
+    with open("./contentful-data/migrations/deletion-changeset.json", "w") as outfile:
+        dump(deletion_changeset, outfile)
