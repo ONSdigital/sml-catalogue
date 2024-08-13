3fa16282996521b468506494ebc1965418843e01
Joseph-Percival-ONS <joseph.percival@ons.gov.uk>
2024-08-13 17:05:11 +0100

    update logs

--- contentful-data/migrations/dev-export.js
+++ contentful-data/migrations/dev-export.js
@@ -1,3 +1,4 @@
 function migrationFunction(migration, context) {
+    migration.deleteContentType("test")
 }
 module.exports = migrationFunction;
--- contentful-data/rollbacks/dev-export.js
+++ contentful-data/rollbacks/dev-export.js
@@ -1,3 +1,30 @@
 function migrationFunction(migration, context) {
+    const test = migration.createContentType("test");
+    test
+        .displayField("test")
+        .name("test")
+        .description("test")
+
+    const testTest = test.createField("test");
+    testTest
+        .name("test")
+        .type("Symbol")
+        .localized(false)
+        .required(false)
+        .validations([])
+        .disabled(false)
+        .omitted(false)
+
+    const testTest1 = test.createField("test1");
+    testTest1
+        .name("test1")
+        .type("Symbol")
+        .localized(false)
+        .required(false)
+        .validations([])
+        .disabled(false)
+        .omitted(false)
+    test.changeFieldControl("test", "builtin", "singleLine")
+    test.changeFieldControl("test1", "builtin", "singleLine")
 }
 module.exports = migrationFunction;
